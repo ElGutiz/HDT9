ad09e07ac744f2832ec26805d07db131323afb77
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-04-26 19:02:18 -0600

    Association, Driver y Factory Class

---
+++ .idea/Association.java
@@ -0,0 +1,30 @@
+/**
+ * Hoja de Trabajo 9
+ * Algoritmos y Estructuras de Datos
+ * Sección: 30
+ * Fecha de entrega: 27/04/2020
+ * Jose Gutierrez	carne: 19111
+ * Cesar Rodas  carne: 16776
+ */
+
+public class Association<K extends Comparable<K>, V> {
+    private K key;
+    private V value;
+
+    public Association(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey(){
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+
+    public int compareTo(Association<K,V> association){
+        return key.compareTo(association.key);
+    }
+}
---
+++ .idea/Factory.java
@@ -0,0 +1,28 @@
+/**
+ * Hoja de Trabajo 9
+ * Algoritmos y Estructuras de Datos
+ * Sección: 30
+ * Fecha de entrega: 27/04/2020
+ * Jose Gutierrez	carne: 19111
+ * Cesar Rodas  carne: 16776
+ */
+
+public class Factory{
+    public Mapping<String,String> getMapping(String type){
+        if(type == null){
+            System.out.println("-Se ha ingresado un valor no valido, por favor ingresar de nuevo...");
+            return null;
+        }
+
+        if(type.equals("HashMap")){
+            return new Hashing<String,String>();
+        }
+
+        else if(type.equals("SplayTree")){
+            return new SplayTreeMapping();
+        }
+
+        System.out.println("-Se ha ingresado un valor no valido, por favor ingresar de nuevo...");
+        return null;
+    }
+}
